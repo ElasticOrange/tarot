01ad1af5945a4909378a5126197629b84db00f16
Alex Urucu <quigonjin2@gmail.com>
2015-07-19 18:20:30 +0300

    Add placeholder for sites list when there are no sites

--- resources/views/site/list.blade.php
+++ resources/views/site/list.blade.php
@@ -29,26 +29,36 @@
 		</div>
 	</div>
 
-	<table class="table table-striped">
-		<thead>
-			<tr>
-				<th>#</th>
-				<th>Name</th>
-				<th class="hidden-xs">URL</th>
-				<th>Active</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php $rowIndex = 1 ?>
-			@foreach ($sites as $site)
-				<tr href="/sites/{{ $site->id }}">
-					<td>{{$rowIndex++}}</td>
-					<td>{{ $site->name }}</td>
-					<td class="hidden-xs">{{ $site->url }}</td>
-					<td> {!! $site->active ? '<span class="glyphicon glyphicon-ok"></span>' : '' !!}</td>
-				</tr>
-			@endforeach
-		</tbody>
 
-	</table>
+	<p>
+		@if (count($sites) > 0)
+			<table class="table table-striped">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Name</th>
+						<th class="hidden-xs">URL</th>
+						<th>Active</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php $rowIndex = 1 ?>
+					@foreach ($sites as $site)
+						<tr href="/sites/{{ $site->id }}">
+							<td>{{$rowIndex++}}</td>
+							<td>{{ $site->name }}</td>
+							<td class="hidden-xs">{{ $site->url }}</td>
+							<td> {!! $site->active ? '<span class="glyphicon glyphicon-ok"></span>' : '' !!}</td>
+						</tr>
+					@endforeach
+				</tbody>
+
+			</table>
+		@else
+			<div class="alert alert-info">
+				<span class="content-placeholder">There are no sites for the moment. Please <strong>add</strong> one!</span>
+			</div>
+		@endif
+	</p>
+
 @endsection

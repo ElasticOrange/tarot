f5568601b045e1b8aab606c948da9cb05956018d
Alex Urucu <quigonjin2@gmail.com>
2015-07-21 10:52:41 +0300

    Enforce login

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,75 +10,80 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
 Route::get('/home', function() {
     return redirect('/');
 });
 
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('/sites/{sites}/delete', 'SiteController@destroy');
+    Route::resource('sites', 'SiteController');
 
-Route::get('/sites/{sites}/delete', 'SiteController@destroy');
-Route::resource('sites', 'SiteController');
+    Route::get('/sites/{sites}/infocosts/{infocosts}/delete', 'InfocostsController@destroy');
+    Route::resource('sites.infocosts', 'InfocostsController');
 
-Route::get('/sites/{sites}/infocosts/{infocosts}/delete', 'InfocostsController@destroy');
-Route::resource('sites.infocosts', 'InfocostsController');
+    Route::get('/users/{users}/delete', 'UsersController@destroy');
+    Route::resource('users', 'UsersController');
 
-Route::get('/users/{users}/delete', 'UsersController@destroy');
-Route::resource('users', 'UsersController');
+    Route::get('profile', 'UsersController@profile');
+    Route::post('profile', 'UsersController@updateProfile');
+
+
+    Route::get('/', function () {
+        return view('questions');
+    });
+
+    Route::get('questions', function () {
+        return view('questions');
+    });
+
+    Route::get('emails', function () {
+        return view('emails');
+    });
+
+    Route::get('clients', function () {
+        return view('clients');
+    });
+
+
+
+
+    Route::get('templates/question', function () {
+        return view('question-templates');
+    });
+
+    Route::get('templates/question/item', function () {
+        return view('question-template');
+    });
+
+    Route::get('templates/email', function () {
+        return view('email-templates');
+    });
+
+    Route::get('templates/email/item', function () {
+        return view('email-template');
+    });
+
+    Route::get('settings', function () {
+        return view('settings');
+    });
+
+
+    Route::get('client', function () {
+        return view('client');
+    });
+
+
+
+});
+
+
 
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
-Route::get('profile', 'UsersController@profile');
-Route::post('profile', 'UsersController@updateProfile');
 
 Route::get('password/email', 'Auth\PasswordController@getEmail');
 Route::post('password/email', 'Auth\PasswordController@postEmail');
 Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
 Route::post('password/reset', 'Auth\PasswordController@postReset');
 
-
-
-
-Route::get('/', function () {
-    return view('questions');
-});
-
-Route::get('questions', function () {
-    return view('questions');
-});
-
-Route::get('emails', function () {
-    return view('emails');
-});
-
-Route::get('clients', function () {
-    return view('clients');
-});
-
-
-
-
-Route::get('templates/question', function () {
-    return view('question-templates');
-});
-
-Route::get('templates/question/item', function () {
-    return view('question-template');
-});
-
-Route::get('templates/email', function () {
-    return view('email-templates');
-});
-
-Route::get('templates/email/item', function () {
-    return view('email-template');
-});
-
-Route::get('settings', function () {
-    return view('settings');
-});
-
-
-Route::get('client', function () {
-    return view('client');
-});
